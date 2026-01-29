b2a526c2a30024afb22cd0de0580f51bbac8921f
KimPhan12 <phanvo711@gmail.com>
2026-01-29 15:42:26 +0700

    Ví dụ đầu tiên
    Chỉ để demo

---
+++ HelloWorld/src/phann/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package phann.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, VÀ ĐANG THỬ NGHIỆM QUẢN LÝ MÃ NGUỒN VỚI GIT");
+	}
+
+}
