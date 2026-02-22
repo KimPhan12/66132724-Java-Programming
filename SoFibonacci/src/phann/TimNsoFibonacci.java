5427dc4e47bc8585c93837babac672a3937cd086
KimPhan12 <phanvo711@gmail.com>
2026-02-22 14:06:32 +0700

    Tìm N số Fibonacci

---
+++ SoFibonacci/src/phann/TimNsoFibonacci.java
@@ -0,0 +1,22 @@
+package phann;
+import java.util.Scanner;
+public class TimNsoFibonacci {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner scanner=new Scanner(System.in);
+		n=scanner.nextInt();
+		long f0=0,f1=1,fn=0;
+		for(int i=0;i<=n;i++) {
+			if(i<2) {
+				System.out.println(i);
+			}
+				else {
+			fn = f0+ f1;
+			f0 = f1;
+			f1 = fn;
+			System.out.println(fn);
+			}
+		}
+	}
+}
