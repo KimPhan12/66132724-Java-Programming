16bc52dafea990ba5ab112fc6b843ef24e613eba
KimPhan12 <phanvo711@gmail.com>
2026-03-03 11:44:09 +0700

    Lập bảng cửu chương

---
+++ BANGCUUCHUONG/src/BANGCCHUONG.java
@@ -0,0 +1,14 @@
+
+public class BANGCCHUONG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<10;i++) {
+			for(int j=0; j<=10;j++) {
+				System.out.printf("%2d x %2d = %3d\n",i,j,(i*j));
+			}
+			System.out.println("___________________________");
+		}
+	}
+
+}
