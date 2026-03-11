52010d5c68582b7f0bf846a5c1dd22cb72b2b7b2
KimPhan12 <phanvo711@gmail.com>
2026-03-11 19:03:56 +0700

    Update DoiTPNP.java

--- DoiNhiPhanThapPhan/src/DoiTPNP.java
+++ DoiNhiPhanThapPhan/src/DoiTPNP.java
@@ -1,4 +1,22 @@
-
+import java.util.Scanner;
 public class DoiTPNP {
+	public class Doi {
+		public static void chuyennp(int x)
+		{
+			if(x <= 1) System.out.print(x);
+			else 
+			{
+				chuyennp(x / 2);
+				System.out.print(x % 2);
+			}
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int n = sc.nextInt();
+			chuyennp(n);
+			sc.close();
+		}
 
+	}
 }
